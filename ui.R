4ae62b48232527cdd780df56119fa779a4f9e1cf
lafona <lafona@protonmail.com>
2017-04-25 08:46:35 -0400

    add explanation of selection and relocates feed history slider above graph

--- ui.R
+++ ui.R
@@ -5,12 +5,14 @@ library(DT)
 ui <- fluidPage(
     h1("Bitshares Witness Performance"),
     verbatimTextOutput(outputId = "last_update"),
+    p(strong("Please select witness or witness from table to highlight feed trace")),
     DT::dataTableOutput(outputId = "witness_summary"),
     #uiOutput("select_witness"),
     #p(strong("Missed Blocks:")),
     #verbatimTextOutput(outputId = "missed_block_summary"),
+    uiOutput("reactive_slider"),
     p(strong("Feed History:")),
-    plotOutput(outputId = "feed_trace",click = "plot_select"),
-    uiOutput("reactive_slider")
+    plotOutput(outputId = "feed_trace",click = "plot_select")
+    
     #verbatimTextOutput(outputId = "info")
 )
